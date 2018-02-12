6e2bbc2811262c50b432863f953349855c48a102
Karina <karina.skorka@gmail.com>
2018-02-12 23:24:31 +0100

    Unit tests.

---
+++ tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 0);
+        }
+    }
+}
